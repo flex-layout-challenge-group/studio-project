f483cc49afa54526ac5b6631f22ef2c1ae2810f3
Rafaela Oliveira <labdarafaela@gmail.com>
2022-04-23 23:00:11 -0400

    Final assignment adjustments

--- script.js
+++ script.js
@@ -11,13 +11,17 @@ const img04 = document.querySelector("#img_04");
 const img05 = document.querySelector("#img_05");
 const img06 = document.querySelector("#img_06");
 //list of checkbox
-const box01 = document.querySelector
+const semiLoc = document.querySelector("#semi-loc");
+const semiEdu = document.querySelector("#semi-edu");
+const semiExp = document.querySelector("#semi-exp");
+const photoId = document.querySelector("#prof-info");
+const prvLoc = document.querySelector("#loc");
+const prvEdu = document.querySelector("#edu");
+const prvExp = document.querySelector("#exp");
 
-
-
-pft.addEventListener("change", addActivityItem, false);
-
-function addActivityItem() {
+pft.addEventListener("change", setProfile);
+//addActivityItem
+function setProfile() {
     console.log(pft.value);
     switch (pft.value) {
         case "01":
@@ -40,9 +44,9 @@ function addActivityItem() {
             img01.style.display = "none";
             img02.style.display = "initial";
             img03.style.display = "none";
-            img04.style.display = "initial";
-            img05.style.display = "initial";
-            img06.style.display = "none";
+            img04.style.display = "none";
+            img05.style.display = "none";
+            img06.style.display = "initial";
             break;
         case "03":
             divPublic.style.display = "none";
@@ -61,7 +65,84 @@ function addActivityItem() {
     }
 }
 
-addActivityItem();
+semiLoc.addEventListener("change", () => {    
+    if (semiLoc.checked) {
+        img01.style.display = "initial";
+        img02.style.display = "none";
+        img03.style.display = "none";
+    } else {
+        img01.style.display = "none";
+        img02.style.display = "initial";
+        img03.style.display = "none";
+    } 
+});
 
-//window.addEventListener("load", start, false);
-//document.location.reload(true);
+semiEdu.addEventListener("change", () => {
+    if (semiEdu.checked) {
+        img05.style.display = "initial";
+        img06.style.display = "none";
+    } else {
+        img05.style.display = "none";
+        img06.style.display = "initial";
+    }
+});
+
+semiExp.addEventListener("change", () => {
+    if (semiExp.checked) {
+        img04.style.display = "initial";
+        img06.style.display = "none";
+    } else {
+        img04.style.display = "none";
+        img06.style.display = "initial";
+    }
+});
+
+
+
+photoId.addEventListener("change", () => {    
+    if (photoId.checked) {
+        img01.style.display = "none";
+        img02.style.display = "initial";
+        img03.style.display = "none";
+    } else {
+        img01.style.display = "none";
+        img02.style.display = "none";
+        img03.style.display = "initial";
+        prvLoc.checked = false;
+    } 
+});
+
+
+prvLoc.addEventListener("change", () => {    
+    if (prvLoc.checked) {
+        img01.style.display = "initial";
+        img02.style.display = "none";
+        img03.style.display = "none";
+    } else {
+        img01.style.display = "none";
+        img02.style.display = "initial";
+        img03.style.display = "none";
+    } 
+});
+
+prvEdu.addEventListener("change", () => {
+    if (prvEdu.checked) {
+        img05.style.display = "initial";
+        img06.style.display = "none";
+    } else {
+        img05.style.display = "none";
+        img06.style.display = "initial";
+    }
+});
+
+prvExp.addEventListener("change", () => {
+    if (prvExp.checked) {
+        img04.style.display = "initial";
+        img06.style.display = "none";
+    } else {
+        img04.style.display = "none";
+        img06.style.display = "initial";
+    }
+});
+
+setProfile();
